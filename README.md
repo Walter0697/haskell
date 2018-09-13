24b2423c29d6009f74dc617b8ebffd4ffb62b22c
Walter Cheng <walterpakwai@live.com>
2018-09-13 15:43:43 -0400

    making it beautiful

--- README.md
+++ README.md
@@ -11,6 +11,8 @@ Some haskell programs for myself to practice, or some problems I saw in lecture 
 - self-implemented list data structure with different functions 
 - based implementation here : http://learnyouahaskell.com/making-our-own-types-and-typeclasses#recursive-data-structures
 
-### apocalyptic number
+### apocalyptic_number.hs
 - if 2^n contains 666, then n will be an apocalyptic number
 - http://mathworld.wolfram.com/ApocalypticNumber.html
+
+### Vampire Number, maybe 
--- apocalyptic_number.hs
+++ apocalyptic_number.hs
@@ -1,3 +1,9 @@
+--- is apocalyptic
+--- Input : Integer
+--- Output : boolean for if it is a apocalyptic number
+isApocalyptic :: Int -> Bool
+isApocalyptic n = containSixSixSix( twoToThePowerOf n )
+
 --- two to the power of 
 --- Input : Integer n
 --- Output : 2^n
@@ -19,12 +25,6 @@ containSixSixSix n
     | (isLessThanTreeDigit n) == True = False
     | (mod n 1000) == 666 = True
     | otherwise = containSixSixSix (quot n 10)
-
---- is apocalyptic
---- Input : Integer
---- Output : boolean for if it is a apocalyptic number
-isApocalyptic :: Int -> Bool
-isApocalyptic n = containSixSixSix( twoToThePowerOf n )
 
 main = undefined
 
