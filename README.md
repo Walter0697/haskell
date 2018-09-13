55da5a7e668ff38dc5d30e3f6d829754723ad016
Walter Cheng <walterpakwai@live.com>
2018-09-13 15:17:52 -0400

    fixed apocalyptic number

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-Just some haskell program for myself to practice haskell
+Some haskell programs for myself to practice, or some problems I saw in lecture 3007 that I would like to implement for fun
+
+### haskellCompile.sh
+- a shell script to help myself compiling or cleanning haskell code to executable
+- ./haskellCompile.sh compile <file_name>
+- ./haskellCompile.sh clean <file_name>
+- (type the file name without .hs)
+- usually it will be good to use ghci with :load <module_name>
 
 ### list.hs
 - self-implemented list data structure with different functions 
 - based implementation here : http://learnyouahaskell.com/making-our-own-types-and-typeclasses#recursive-data-structures
 
 ### apocalyptic number
-- there is error in this one :)
+- if 2^n contains 666, then n will be an apocalyptic number
+- http://mathworld.wolfram.com/ApocalypticNumber.html
--- apocalyptic_number.hs
+++ apocalyptic_number.hs
@@ -1,24 +1,24 @@
 --- two to the power of 
 --- Input : Integer n
 --- Output : 2^n
-twoToThePowerOf :: (RealFloat num) => Int -> num
+twoToThePowerOf :: (Integral num) => Int -> num
 twoToThePowerOf 0 = 1
 twoToThePowerOf n = 2 * twoToThePowerOf(n - 1)
 
 --- is less than three digits
 --- Input : Number
 --- Output : boolean for is the number less than three digits
-isLessThanTreeDigit :: (RealFloat num) => num  -> Bool
-isLessThanTreeDigit n = (n / 10) < 10
+isLessThanTreeDigit :: (Integral num) => num  -> Bool
+isLessThanTreeDigit n = (quot n 10) < 10
 
 --- contain six six six
 --- Input : Number
 --- Output : boolean for if the number contains 666 
-containSixSixSix :: (RealFloat num) => num -> Bool
+containSixSixSix :: (Integral num) => num -> Bool
 containSixSixSix n 
     | (isLessThanTreeDigit n) == True = False
-    | (mod (round n) 1000) == 666 = True
-    | otherwise = containSixSixSix (n / 10)
+    | (mod n 1000) == 666 = True
+    | otherwise = containSixSixSix (quot n 10)
 
 --- is apocalyptic
 --- Input : Integer
